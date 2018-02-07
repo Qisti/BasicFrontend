efa07aadeb34bfb9e80da17aedbf01d816046cf2
Qisti <36026889+Qisti@users.noreply.github.com>
2018-02-07 16:15:56 +0700

    Create Formulir.js

---
+++ Formulir.js
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Registrasi</title>
+</head>
+<body>
+	
+	<h1 align="center">Form Registrasi</h1>
+
+	<form> 
+		<table>
+			<tr>
+			<td>Nama</td>
+			<td> :</td> 
+			<td><input type="text" name="nama" value=""/></td>
+			</tr>
+			<tr>
+			<td>Jenis Kelamin</td> 
+			<td> :</td>
+			<td><input type="radio" name="gender" value="laki">L
+			<input type="radio" name="gender" value="perempuan"> P<br> </td>
+			</tr>
+			<tr>
+			<td>Tanggal Lahir</td>
+			<td>:</td>
+			<td><input type="text" name="nama" value=""/></td>	
+			</tr>
+			<tr>
+			<td>Alamat</td>
+			<td>:</td>
+			<td><input type="textarea" name="" style="width:500px; height:100px"></td>
+			</tr>
+			<tr><td><input type="submit" value="Submit"></td></tr>		
+		</table>
+	</form>
+
+</body>
+</html>
